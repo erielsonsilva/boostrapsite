1b1bef6e66079ca669ac2dc2000ef0993a85fe54
erielsonsilva <erielson.silva@yahoo.com>
2022-06-28 21:02:38 -0300

    atualizacao scrip

---
+++ scrip.js
@@ -0,0 +1,8 @@
+function teste() {
+    // let email = document.getElementById('inputEmail14').value;
+    // console.log(`Email: ${email}`);
+    const form = document.getElementsByClassName('row g-3')[0]
+    const data = new FormData(form);
+    const formJSON = Object.fromEntries(data.entries()); 
+    console.log(formJSON);
+}   
